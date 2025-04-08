9017bbdfb7ad95227f8b0c61f6fae40f10432cee
abdogarcia <agarcia@ieseljust.com>
2025-04-08 08:56:43 +0200

    47 commit intent de posar profe i assig

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import paquet1.Alumne;
 
+import java.util.ArrayList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -13,6 +15,9 @@ public class Main {
         return nom + " " + edat;
     }
     public static void main(String[] args) {
+        ArrayList<String> noms = new ArrayList();
+
+        noms.add("111");
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
